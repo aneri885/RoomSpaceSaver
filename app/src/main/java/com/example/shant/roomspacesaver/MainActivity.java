c596da45cb40a87c8da52ba20864157fc44490f8
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-10-24 14:13:28 -0400

    removed registration for the time being

--- app/src/main/java/com/example/shant/roomspacesaver/MainActivity.java
+++ app/src/main/java/com/example/shant/roomspacesaver/MainActivity.java
@@ -41,35 +41,35 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout signUpLayout = (LinearLayout) findViewById(R.id.signUpView);
         final EditText newUserName = (EditText) findViewById(R.id.newUserNameText);
         final EditText newUserPass = (EditText) findViewById(R.id.newUserPasswordText);
-        Log.d("New user!","Signup");
-        Button signUpButton = (Button) findViewById(R.id.signUpButton);
-        loginlayout.setVisibility(View.INVISIBLE);
-        signUpLayout.setVisibility(View.VISIBLE);
+//        Log.d("New user!","Signup");
+//        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+//        loginlayout.setVisibility(View.INVISIBLE);
+//        signUpLayout.setVisibility(View.VISIBLE);
         //User newUser = new User();
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                String userName = newUserName.getText().toString();
-                String userPass = newUserPass.getText().toString();
-                Log.d("Username", userName);//if both are blank, logs dont work
-                Log.d("Password",userPass);//if both are blank, logs dont work
-                Log.d("Signup Button","Clicked");
-                //create new user
-                User newUser = new User();
-                newUser.userName = userName;
-                newUser.password = userPass;
-                newUser.rooms= new Room[0];
-                boolean inserted = myDb.insertUser(userName, userPass, "[]");
-                if (inserted){
-                    Toast.makeText(MainActivity.this, "User created, Please login", Toast.LENGTH_SHORT).show();
-                    loginlayout.setVisibility(View.VISIBLE);
-                    signUpLayout.setVisibility(View.INVISIBLE);
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "User creation failed, retry again", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        signUpButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//                String userName = newUserName.getText().toString();
+//                String userPass = newUserPass.getText().toString();
+//                Log.d("Username", userName);//if both are blank, logs dont work
+//                Log.d("Password",userPass);//if both are blank, logs dont work
+//                Log.d("Signup Button","Clicked");
+//                //create new user
+//                User newUser = new User();
+//                newUser.userName = userName;
+//                newUser.password = userPass;
+//                newUser.rooms= new Room[0];
+//                boolean inserted = myDb.insertUser(userName, userPass, "[]");
+//                if (inserted){
+//                    Toast.makeText(MainActivity.this, "User created, Please login", Toast.LENGTH_SHORT).show();
+//                    loginlayout.setVisibility(View.VISIBLE);
+//                    signUpLayout.setVisibility(View.INVISIBLE);
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this, "User creation failed, retry again", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
     }
 }
