4996d514f4410d2cf7f1d534ebc866e4ad16c7e7
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-11-18 20:18:18 -0500

    fixed weird permissions bug, creating users, rooms, furnitures table

--- app/src/main/java/com/example/shant/roomspacesaver/DBHelper.java
+++ app/src/main/java/com/example/shant/roomspacesaver/DBHelper.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.LoginFilter;
 import android.util.Log;
 
 /**
@@ -14,27 +15,48 @@ import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper{
     public final static String DATABASE_NAME = "appData.db";
-    public final static String TABLE_NAME = "users";
-    public final static String COL_1 = "ID";
-    public final static String COL_2 = "USERNAME";
-    public final static String COL_3 = "PASSWORD";
-    public final static String COL_4 = "ROOM_IDS";
+    //create 3 required tables
+    public final static String USERS_TABLE = "users";
+    public final static String ROOMS_TABLE = "rooms";
+    public final static String FURNITURES_TABLE = "furniture";
+    //common variable id for all tables
+    public final static String ID = "ID";
+    // users table
+    public final static String USERNAME = "USERNAME";
+    public final static String PASSWORD = "PASSWORD";
+    public final static String ROOM_IDS = "ROOM_IDS";
+    //rooms table
+    public final static String ROOM_LENGTH = "ROOM_LENGTH";
+    public final static String ROOM_WIDTH = "ROOM_WIDTH";
+    public final static String FURNITURE_IDS = "FURNITURE_IDS";
+    //furnitures table
+    public final static String FURNITURE_LENGTH = "FURNITURE_LENGTH";
+    public final static String FURNITURE_WIDTH = "FURNITURE_WIDTH";
+    public final static String X_POSITION = "X_POSITION";
+    public final static String Y_POSITION = "Y_POSITION";
 
     // contructor to create table
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        //SQLiteDatabase db = this.getReadableDatabase(); just used to check if db was created when constructor was called
+        //SQLiteDatabase db = this.getReadableDatabase();// just used to check if db was created when constructor was called
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         //create table whenever class is called, takes is SQLiteDatabse class
-        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, USERNAME TEXT, PASSWORD TEXT, ROOM_IDS TEXT)");//Exposes methods to manage a SQLite database.
+        // create all  tables i.e users, rooms, furnitures
+        Log.d("hey","there on create");
+        db.execSQL("create table " + USERS_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, USERNAME TEXT, PASSWORD TEXT, ROOM_IDS TEXT)");//Exposes methods to manage a SQLite database.
+        db.execSQL("create table " + ROOMS_TABLE +  " (ID INTEGER PRIMARY KEY AUTOINCREMENT, ROOM_LENGTH TEXT, ROOM_WIDTH TEXT, FURNITURE_IDS TEXT)");
+        db.execSQL("create table " + FURNITURES_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, FURNITURE_LENGTH TEXT, FURNITURE_WIDTH TEXT, X_POSITION TEXT, Y_POSITION TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if extists" + TABLE_NAME);
+        Log.d("hey","there on upgrade");
+        db.execSQL("drop table if exists" + USERS_TABLE);
+        db.execSQL("drop table if exists" + ROOMS_TABLE);
+        db.execSQL("drop table if exists" + FURNITURES_TABLE);
         onCreate(db);
 
     }
@@ -42,22 +64,25 @@ public class DBHelper extends SQLiteOpenHelper{
     public boolean insertUser(String username, String password, String room_ids){
         SQLiteDatabase db = this.getReadableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,username);
-        contentValues.put(COL_3,password);
-        contentValues.put(COL_4,room_ids);
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        contentValues.put(USERNAME,username);
+        contentValues.put(PASSWORD,password);
+        contentValues.put(ROOM_IDS,room_ids);
+        long result = db.insert(USERS_TABLE, null, contentValues);
         return result == -1 ? false : true;
     }
 //
 //    public Cursor getUsers(){
 //        SQLiteDatabase myDb = this.getWritableDatabase();
-//        Cursor result = myDb.rawQuery("select * from " + TABLE_NAME, null);
+//        Cursor result = myDb.rawQuery("select * from " + USERS_TABLE, null);
 //        return result;
 //    }
 
     public boolean checkCredentials(String username,String password){
         Log.d("Inside ","checkCredentials");
+        /*was getting an error: unable to open databsse file at line below, solution chmod 777 from terminal (data/data/appname/databases)
+         to appData.db and appData.db-journal files, file permission error */
         SQLiteDatabase myDb = this.getWritableDatabase();
+        Log.d(username,password);
         Cursor result = myDb.rawQuery("select username,password from users where username=? and password=?",new String[]{username,password});//in java single quotes can take only once charater
         Log.d("result count: ", String.valueOf(result.getCount()));
         Log.d("column count: ", String.valueOf(result.getColumnCount()));
